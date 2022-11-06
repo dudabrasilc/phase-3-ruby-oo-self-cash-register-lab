32bab1a340cc286f81e2de81b196dc452dcbfdd9
Maria Coitinho <brasildu@gmail.com>
2022-11-06 01:25:24 -0600

    commit tree 18b9ff4b4596880c9df10ac2459cf6e0c89a1120

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total, :title
+
+  @@all = []
+  def initialize(discount = 20)
+    @discount = discount
+    @total = 0
+  end
+
+  def add_item(title, price, quantity = 1)
+    @@all << title
+    self.total += price * quantity
+  end
+
+  def apply_discount
+    if 0 == @discount
+      "There is no discount to apply."
+    else
+      discount_percent = 1 - @discount/100.to_f
+      @total = @total * discount_percent
+      "After the discount, the total comes to $#{@total.round}."
+    end
+  end
+
+  def items()
+    @@all
+  end
+
+
+end
+
+c1 = CashRegister.new
+c1.add_item("macbook air", 1000)
+c1.add_item("iphone", 400)
+# c2 = CashRegister.new
+# c2.add_item("iphone", 500)
+# c1.apply_discount
+# c1.items("iphone", 500)
+# c1.items("macbook air", 1000)
+binding.pry
